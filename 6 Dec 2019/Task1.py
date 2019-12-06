847c4db010de96a7804db2d1369be9c5d02a36a9
Allen Biju Thomas <allenbijuthomas@gmail.com>
2019-12-06 23:46:44 +0530

    String py file pushed

---
+++ 6 Dec 2019/Task1.py
@@ -0,0 +1,10 @@
+str = '''William Shakespeare (bapt. 26 April 1564 – 23 April 1616)[a] was an English poet, playwright, and actor, 
+widely regarded as the greatest writer in the English language and the world's greatest dramatist.[2][3][4] 
+He is often called England's national poet and the Bard of Avon (or simply the Bard).[5][b] His extant works,
+including collaborations, consist of some 39 plays,[c] 154 sonnets, two long narrative poems, and a few other
+verses, some of uncertain authorship.'''
+
+strhindi = 'कृपया प्रतीक्षा करें, हिंदी भाषा का अनुवाद लोड हो रहा है ..'
+
+print(str)
+print(strhindi)
