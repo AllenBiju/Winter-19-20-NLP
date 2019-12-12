f4527122db125e1cc3a4d47ec0180d23d4fd7ebb
Allen Biju Thomas <allenbijuthomas@gmail.com>
2019-12-12 23:36:47 +0530

    11 Dec Task

---
+++ 11 Dec 2019/chinese_word_segment.py
@@ -0,0 +1,11 @@
+import jieba
+
+ch_text = "当前，世界面临食品、气候变化以及金融等多重危机，而正是这些危机更加突出了农业在发展中国家至关重要的地位"
+word_list = jieba.cut(ch_text, cut_all=False)
+
+# for i in word_list:
+#     print(i)
+
+segmented_text = "  ".join(word_list)
+
+print(segmented_text)
---
+++ 11 Dec 2019/language_features.py
@@ -0,0 +1,15 @@
+# Orthography – 
+# At its most basic, an orthography is a standardized system of writing. 
+# It's the collection of rules that lets us visually represent a language. 
+# This starts with creating the symbols upon which a written language is built. 
+# Example – logographic orthography uses use symbols to represent entire words or ideas.
+
+# Suprasegmental
+# Suprasegmental features are speech features that describe phonetic features. 
+# They describe the way a word is pronounced. 
+# They have features such as stress, tone, or word juncture over particular words, phrases or syllables.
+
+# Typographic Features –
+# Typographic features are the visual features of the font of a particular language. 
+# These features are used to make the words more legible, readable and appealing. 
+# The arrangement of type involves selecting typefaces, point sizes, line lengths, line-spacing (leading), and letter-spacing (tracking), and adjusting the space between pairs of letters.
